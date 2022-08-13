06c60bc44583a54f81d47af476118e57a4353afb
Madhav Paudel <poudelmadhav143@gmail.com>
2022-08-13 18:03:28 +0545

    update total of line item with turbo

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,4 +6,8 @@ class LineItem < ApplicationRecord
   validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   delegate :quote, to: :line_item_date
+
+  def total_price
+    quantity * unit_price
+  end
 end
